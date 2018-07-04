808883abd0da079f7efcb76699b837fd0b08cbe6
jailson santos <jailson_donald_cobain@hotmail.com>
2018-07-03 23:33:58 -0300

    trabalhando com variaveis

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -14,7 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
-
+  public nomeUsuario:string = "Charles Franca";
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
